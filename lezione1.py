3b84715c7c4afb18b3404da5bc0f7209df12ca20
Gabriele <94189917+gabry1242@users.noreply.github.com>
2021-11-16 16:56:57 +0000

    commenti

--- lezione1.py
+++ lezione1.py
@@ -2,10 +2,11 @@
 def somma(lista):
     #inizializzo il risultato
     risultato=0
+    #leggi ciascun elemento e sommalo
     for item in my_list:
         risultato=risultato+item
     return risultato
-
+#inserisco la mia lista
 my_list=[1, 4, 78, 43, 92, 143]
 #leggere tutti gli elementi della lista e sommarli 
 print (somma(my_list))
