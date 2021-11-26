287dc9dcf5379fa8d762d9c568638082d8e3cc87
Gabriele <94189917+gabry1242@users.noreply.github.com>
2021-11-26 14:59:02 +0000

    correzione di errore con una lista

---
+++ somma.py
@@ -0,0 +1,17 @@
+#creare una lista
+valori=[]
+#apro e leggo il file txt
+lista=open('sales.txt','r')
+
+#divido gli elementi per prendere i valori e non le date
+for line in lista:
+    oggetto=line.split(',')
+
+    if oggetto[1]!='Sales\n':
+        
+        valori.append(float(oggetto[1]))
+
+
+#sommao tutti i valori
+listsum=sum(valori)
+print(listsum)
